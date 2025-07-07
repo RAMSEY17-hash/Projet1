12422603bc33b217fbcfc504b3621b8b97d276cb
RAMSEY17-hash <wouemboahmed@gmail.com>
2025-07-07 10:30:41 +0000

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 
 # ------------------- IMPORTS ET CONFIGURATION -------------------
 import sqlite3
-import logging
-logging.basicConfig(level=logging.INFO)
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
